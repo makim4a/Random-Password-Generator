0d16ee7e68e43ca3bab17b94362a21123baf9ae7
makim4a <153382202+makim4a@users.noreply.github.com>
2026-04-27 14:38:06 +0300

    Delete history.py

--- history.py
+++
@@ -1,4 +0,0 @@
-    def save_history(self):
-        try:
-            with open(self.history_file, 'w', encoding='utf-8') as f:
-                json.dump(self.password_history, f, ensure_ascii=False, indent=2)
